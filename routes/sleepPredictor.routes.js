13077886995290968128a70866c6ada827d4a7ac
Shehan Chanuka <shehanxdev@gmail.com>
2025-03-16 14:56:19 +0530

    feature: added update and delete enfpoints

--- routes/sleepPredictor.routes.js
+++ routes/sleepPredictor.routes.js
@@ -3,11 +3,15 @@ const {
   sleepPredictor,
   addSleepRecord,
   getAllSleepRecords,
+  updateRecord,
+  deleteRecord,
 } = require("../controllers/sleepPredictor.controller");
 const router = express.Router();
 
 router.post("/predict", sleepPredictor);
 router.post("/addRecord", addSleepRecord);
 router.get("/getRecords", getAllSleepRecords);
+router.put("/update/:id", updateRecord);
+router.delete("/delete/:id", deleteRecord);
 
 module.exports = router;
